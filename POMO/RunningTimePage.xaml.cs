4d0ef4e544dc97848cc65dad9471b2a16058fb64
Renzo Osmillo <osmillo08@gmail.com>
2024-12-23 14:51:54 +0800

    Added the SkipPopUp in RunningTime

--- POMO/RunningTimePage.xaml.cs
+++ POMO/RunningTimePage.xaml.cs
@@ -6,7 +6,10 @@ namespace POMO
 		public RunningTimePage()
 		{
 			InitializeComponent();
-		}
+
+            // Listen for the SkipConfirmed event from the SkipPopUp
+            SkipPopup.SkipConfirmed += OnSkipConfirmed;
+        }
 
         private async void OnHomeButtonTapped(object sender, EventArgs e)
         {
@@ -18,6 +21,17 @@ namespace POMO
         {
             // Navigate to TimerPage
             await Shell.Current.GoToAsync("TimerPage");
+        }
+
+        private void OnSkipButtonTapped(object sender, EventArgs e)
+        {
+            SkipPopup.IsVisible = true; // Show the pop-up
+        }
+
+        private void OnSkipConfirmed(object? sender, EventArgs e)
+        {
+            // Logic when the skip is confirmed
+            DisplayAlert("Skipped", "You skipped the current focus session.", "OK");
         }
     }
 }
---
+++ POMO/SkipPopUp.xaml.cs
@@ -0,0 +1,26 @@
+namespace POMO
+{
+	public partial class SkipPopUp : ContentView
+	{
+        // Declare the event as nullable
+        public event EventHandler? SkipConfirmed;
+
+        public SkipPopUp()
+		{
+            InitializeComponent();  
+		}
+
+        private void OnCancelTapped(object sender, EventArgs e)
+        {
+            this.IsVisible = false; // Hide the pop-up
+        }
+
+        private async void OnSkipConfirmedTapped(object sender, EventArgs e)
+        {
+            SkipConfirmed?.Invoke(this, EventArgs.Empty); // Notify the parent page
+            this.IsVisible = false; // Hide the pop-up
+            // Navigate to TimerPage
+            await Shell.Current.GoToAsync("TimerPage");
+        }
+    }
+}
